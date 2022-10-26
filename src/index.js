30324a6a0a7ded7421e197c2708827f9da3ad880
Dima Samoylik <samoylikd@gmail.com>
2022-10-26 17:41:38 +0300

    HW-11
    HW-11

---
+++ src/index.js
@@ -0,0 +1,104 @@
+import './css/style.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import ImagesApiService from './js/fetch';
+
+const searchForm = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadMoreRef = document.querySelector('.load-more');
+const lightbox = new SimpleLightbox('.gallery a');
+
+const imagesApiService = new ImagesApiService();
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreRef.addEventListener('click', onLoadMore);
+
+loadMoreRef.classList.add('is-hidden');
+
+function onSearch(event) {
+  event.preventDefault();
+  clearHitsContainer();
+  imagesApiService.searchQuery =
+    event.currentTarget.elements.searchQuery.value.trim();
+  imagesApiService.resetPage();
+  if (imagesApiService.searchQuery === '') {
+    Notiflix.Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    return;
+  }
+  imagesApiService.fetchHits().then(({ data }) => {
+    // console.log(data.total);
+    if (data.total === 0) {
+      Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+      return;
+    }
+
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images`);
+    appendMarckup(data);
+    loadMoreRef.classList.remove('is-hidden');
+    lightbox.refresh();
+  });
+}
+
+function onLoadMore() {
+  imagesApiService.fetchHits().then(({ data }) => {
+    const totalPages = Math.ceil(data.totalHits / imagesApiService.perPage);
+    if (imagesApiService.page > totalPages) {
+      loadMoreRef.classList.add('is-hidden');
+      Notiflix.Notify.info(
+        `We're sorry, but you've reached the end of search results.`
+      );
+    }
+    appendMarckup(data);
+
+    lightbox.refresh();
+  });
+}
+
+function appendMarckup(data) {
+  galleryRef.insertAdjacentHTML('beforeend', createMarckup(data));
+}
+
+function createMarckup({ hits }) {
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+  <div class="photo-card">
+  <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>
+  `
+    )
+    .join('');
+}
+
+function clearHitsContainer() {
+  galleryRef.innerHTML = '';
+}
---
+++ src/js/fetch.js
@@ -0,0 +1,29 @@
+const axios = require('axios').default;
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 40;
+  }
+
+  async fetchHits() {
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=30633166-da637d447e23da33de67e6409&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`
+      );
+      this.incrementPage();
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
